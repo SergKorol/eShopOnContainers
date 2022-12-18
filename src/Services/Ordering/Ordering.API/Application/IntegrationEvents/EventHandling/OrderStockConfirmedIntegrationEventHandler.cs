a3637ed85790be4253e88ee8131eadb288442e51
SergKorol <korol_sergey@me.com>
2022-12-18 23:33:05 +0200

    package fix

---
+++ src/Services/Ordering/Ordering.API/Application/IntegrationEvents/EventHandling/OrderStockConfirmedIntegrationEventHandler.cs
@@ -0,0 +1,46 @@
+﻿namespace Ordering.API.Application.IntegrationEvents.EventHandling
+{
+    using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Abstractions;
+    using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Extensions;
+    using System.Threading.Tasks;
+    using Events;
+    using MediatR;
+    using System;
+    using Commands;
+    using Microsoft.Extensions.Logging;
+    using Serilog.Context;
+
+    public class OrderStockConfirmedIntegrationEventHandler :
+        IIntegrationEventHandler<OrderStockConfirmedIntegrationEvent>
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<OrderStockConfirmedIntegrationEventHandler> _logger;
+
+        public OrderStockConfirmedIntegrationEventHandler(
+            IMediator mediator,
+            ILogger<OrderStockConfirmedIntegrationEventHandler> logger)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Handle(OrderStockConfirmedIntegrationEvent integrationEvent)
+        {
+            using (LogContext.PushProperty("IntegrationEventContext", $"{integrationEvent.Id}-{Program.AppName}"))
+            {
+                _logger.LogInformation("----- Handling integration event: {IntegrationEventId} at {AppName} - ({@IntegrationEvent})", integrationEvent.Id, Program.AppName, integrationEvent);
+
+                var command = new StockConfirmedCommand(integrationEvent.OrderId);
+
+                _logger.LogInformation(
+                    "----- Sending command: {CommandName} - {IdProperty}: {CommandId} ({@Command})",
+                    command.GetGenericTypeName(),
+                    nameof(command.OrderNumber),
+                    command.OrderNumber,
+                    command);
+
+                await _mediator.Send(command);
+            }
+        }
+    }
+}
